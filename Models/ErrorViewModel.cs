b5eb2f6888d61463673e9c10ad2814a97e9ca02c
Keran Barbieri <keranbarbieri@hotmail.com>
2021-07-23 19:10:37 -0300

    Created Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VarejaoDoNene.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
